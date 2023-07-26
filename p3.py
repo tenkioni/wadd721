8ff22e557d51f35f580cf29f06ef9d56a43d8d26
tenkioni <138882160+tenkioni@users.noreply.github.com>
2023-07-26 19:03:24 +0800

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Canvas
+frame=Tk()
+canvas=Canvas(bg='white',height=250,width=300)
+
+canvas.create_rectangle(50, 60, 220, 160, fill='Darkgreen',
+                        outline='darkkhaki')
+
+canvas.create_text(100, 100, text='Henry Chan',
+                   anchor='nw',font='italic',fill='white')
+
+canvas.create_line(100, 115, 178, 115)
+canvas.pack()
+frame.mainloop()
