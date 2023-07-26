14b91042beacbfdc285b7ca2576c37c69c30d082
tenkioni <138882160+tenkioni@users.noreply.github.com>
2023-07-26 21:01:18 +0800

    Add files via upload

---
+++ triangle-fail-result.py
@@ -0,0 +1,8 @@
+============= RESTART: C:/Users/User.LAB331E/Documents/triangle.py =============
+Please enter coordinate x1: 100
+Please enter coordinate y1: 100
+Please enter coordinate x2: 100
+Please enter coordinate y2: 100
+Please enter coordinate x3: 100
+Please enter coordinate y3: 100
+Does not form a triangle
