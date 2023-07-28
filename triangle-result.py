d2a7c11c0cf7c978022baaa3ec80d446ad6d45d1
tenkioni <138882160+tenkioni@users.noreply.github.com>
2023-07-28 19:20:57 +0800

    Delete triangle-result.py

--- triangle-result.py
+++
@@ -1,8 +0,0 @@
-= RESTART: C:/Users/User.LAB331E/AppData/Local/Programs/Python/Python310/triangle.py
-Please enter coordinate x1: 100
-Please enter coordinate y1: 10
-Please enter coordinate x2: 50
-Please enter coordinate y2: 100
-Please enter coordinate x3: 100
-Please enter coordinate y3: 100
-Area of your triangle is 2250.0
