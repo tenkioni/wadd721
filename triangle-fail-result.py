cdad0e0772a3820a27f7a31d52aea33accebb7b8
tenkioni <138882160+tenkioni@users.noreply.github.com>
2023-07-28 19:20:50 +0800

    Delete triangle-fail-result.py

--- triangle-fail-result.py
+++
@@ -1,8 +0,0 @@
-============= RESTART: C:/Users/User.LAB331E/Documents/triangle.py =============
-Please enter coordinate x1: 100
-Please enter coordinate y1: 100
-Please enter coordinate x2: 100
-Please enter coordinate y2: 100
-Please enter coordinate x3: 100
-Please enter coordinate y3: 100
-Does not form a triangle
