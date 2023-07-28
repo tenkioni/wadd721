ab33d14a7ae1154ad810acd5d3e77a122f94c092
tenkioni <138882160+tenkioni@users.noreply.github.com>
2023-07-28 19:19:56 +0800

    Delete circle-result.py

--- circle-result.py
+++
@@ -1,3 +0,0 @@
-============== RESTART: C:/Users/User.LAB331E/Documents/circle.py ==============
-Enter radius in feet : 100
-The radius you provided was 100.00 feet and the area is about 31415.93 sq feet
